c90f022142feb66d9464c85b9b229f7df77ee721
Hosein <48409432+Ho3einWave@users.noreply.github.com>
2023-04-21 18:25:55 +0330

    onlynudes text logo added

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -12,11 +12,12 @@ export default function Content() {
   return (
     <div className="w-full h-full fixed -z-[2] grid place-items-center left-0 bg-black bg-opacity-60 text-white">
       <div className="flex flex-col items-center gap-2 mb-32 sm:mx-2">
+        <img src="/assets/logotext.png" alt="Onlynudes Text Logo" />
         <h1 className="text-3xl font-bold sm:text-center xsm:text-2xl">
-          The most interactive NSFW Discord Bot
+          The Most Interactive NSFW Discord Bot
         </h1>
         <p className="xsm:text-sm text-center">
-          No bullshit, just a shit ton of content with HQ Quality
+          Get HQ Content Galore - Videos, Articles & More!
         </p>
         <div className="flex items-center">
           <button
